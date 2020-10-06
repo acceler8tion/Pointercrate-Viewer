7cf05e5750b35926c945d262078745013693c233
DenFade <rurchi1206@gmail.com>
2020-10-07 00:29:32 +0900

    Prepare [1.0.0] Nothing changed. just for sync

--- app/src/main/java/com/github/acceler8tion/pointercrateviewer/adapter/DemonListAdapter.kt
+++
@@ -1,5 +0,0 @@
-package com.github.acceler8tion.pointercrateviewer.adapter
-
-class DemonListAdapter {
-    //TODO: need to extend RecyclerView.Adapter<DemonListAdapter.ViewHolder>
-}
---
+++ app/src/main/java/com/github/acceler8tion/pointercrateviewer/adapter/MainListAdapter.kt
@@ -0,0 +1,27 @@
+package com.github.acceler8tion.pointercrateviewer.adapter
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class MainListAdapter : RecyclerView.Adapter<MainListAdapter.ViewHolder>() {
+
+    class ViewHolder : RecyclerView.ViewHolder {
+
+        constructor(itemView: View) {
+            TODO("Build later...")
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
