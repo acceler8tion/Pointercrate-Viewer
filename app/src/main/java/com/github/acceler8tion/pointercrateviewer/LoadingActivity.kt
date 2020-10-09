e08e007a6a78fc58a507f5314c1676f7f9acb91d
DenFade <rurchi1206@gmail.com>
2020-10-09 12:53:45 +0900

    Prepare [1.0.0] Created LoadingActivity

---
+++ app/src/main/java/com/github/acceler8tion/pointercrateviewer/LoadingActivity.kt
@@ -0,0 +1,11 @@
+package com.github.acceler8tion.pointercrateviewer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoadingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_loading)
+    }
+}
